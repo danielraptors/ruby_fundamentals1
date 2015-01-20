70ab61508298e13d0888addc9700b8b50a451fa4
danielcarter <carter_d89@hotmail.com>
2015-01-20 15:41:39 -0500

    E2

---
+++ exercise2.rb
@@ -0,0 +1,23 @@
+#First Question
+
+meal = 55 
+
+puts "#{meal * 0.15} is a good tip" 
+
+#Second Question 
+
+String = "T"
+
+Integer = 9
+
+puts "#{String + Integer.to_s}" 
+
+#Third Question 
+
+x = 45628 * 7839
+
+puts "#{x}"
+
+#Fourth Question 
+
+True
